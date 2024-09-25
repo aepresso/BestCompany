aeebd944cc2f35709f63f803359d3d3e9745604d
aepresso <isaacmine512@gmail.com>
2024-09-24 23:00:38 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       int customerAge = 32;
+       String customerName = "Bob";
+       double customerWeight = 205.8;
+       String customerHobby = "Golf";
+
+       System.out.println("The customer's name is " + customerName + " he is " + customerAge +
+               " years old he weighs: " + customerWeight + " pounds and his hobby is " + customerHobby);
+    }
+}
